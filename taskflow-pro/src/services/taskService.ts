b023162a3eab93b48ecce52444415a3855e1eb02
Gauravkumar215 <gr5784039@gmail.com>
2026-05-02 02:34:48 +0530

    Update taskService.ts

--- taskflow-pro/src/services/taskService.ts
+++ taskflow-pro/src/services/taskService.ts
@@ -1,12 +1,17 @@
-import api from "../../../../taskflow-pro12/Team Task Manager/taskflow-pro/src/services/api";
+import api from "./api";
 
 export const taskService = {
   getProjectTasks: (projectId: string) =>
     api.get(`/projects/${projectId}/tasks`),
+
   createTask: (projectId: string, data: any) =>
     api.post(`/projects/${projectId}/tasks`, data),
+
   getTask: (taskId: string) => api.get(`/tasks/${taskId}`),
+
   updateTask: (taskId: string, data: any) =>
     api.patch(`/tasks/${taskId}`, data),
-  deleteTask: (taskId: string) => api.delete(`/tasks/${taskId}`),
+
+  deleteTask: (taskId: string) =>
+    api.delete(`/tasks/${taskId}`),
 };
